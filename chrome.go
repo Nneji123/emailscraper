09793187869750fbce21a9a4c5db58b4796106a4
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2024-03-21 17:05:53 +0100

    Update chrome.go

--- chrome.go
+++ chrome.go
@@ -1,44 +1,35 @@
 package emailscraper
 
 import (
-	"context"
-	"fmt"
-	"time"
+    "context"
+    "fmt"
+    "time"
 
-	"github.com/chromedp/chromedp"
-	"github.com/gocolly/colly/v2"
+    "github.com/go-rod/rod"
+    "github.com/go-rod/rod/lib/launcher"
+    "github.com/gocolly/colly/v2"
 )
 
-func initiateScrapingFromChrome(response *colly.Response, timeout int) error {
-	opts := []chromedp.ExecAllocatorOption{
-		//nolint:lll // allow longer line here
-		chromedp.UserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3830.0 Safari/537.36"),
-		//nolint:gomnd // allow magic number here
-		chromedp.WindowSize(1920, 1080),
-		chromedp.NoFirstRun,
-		chromedp.Headless,
-		chromedp.DisableGPU,
-	}
+func initiateScrapingFromRod(response *colly.Response, timeout int) error {
+    ctx := context.Background()
 
-	ctx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
-	defer cancel()
+    // Launch Rod browser with options
+    browser := rod.New().ControlURL(launcher.New().Headless(true).MustLaunch())
 
-	ctx, cancel = chromedp.NewContext(ctx)
-	defer cancel()
+    // Handle timeout
+    if timeout > 0 {
+        ctx, _ = context.WithTimeout(ctx, time.Duration(timeout)*time.Second)
+    }
 
-	if timeout > 0 {
-		ctx, cancel = context.WithTimeout(ctx, time.Duration(timeout)*time.Second)
-		defer cancel()
-	}
+    page := browser.MustPage(response.Request.URL.String())
 
-	var res string
-	if err := chromedp.Run(ctx, chromedp.Navigate(response.Request.URL.String()),
-		chromedp.InnerHTML("html", &res), // Scrape whole rendered page
-	); err != nil {
-		return fmt.Errorf("executing chromedp: %w", err)
-	}
+    // Get the HTML content of the page
+    html, err := page.HTML()
+    if err != nil {
+        return fmt.Errorf("error getting HTML: %w", err)
+    }
 
-	response.Body = []byte(res)
+    response.Body = []byte(html)
 
-	return nil
+    return nil
 }
